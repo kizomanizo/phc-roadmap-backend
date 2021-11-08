00be3e7c1423c25bdc67876367ba752d98825a6d
kizomanizo <kizomanizo@gmail.com>
2021-11-08 20:49:46 +0300

    Initial scaffolding

--- config/settings.py
+++ config/settings.py
@@ -170,9 +170,9 @@ INTERNAL_IPS = [
     # ...
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
+CORS_ORIGIN_WHITELIST = (   
     'http://localhost:8080',
+    'http://localhost:8082',
     'http://localhost:8081',
     'http://localhost:8000',
     'https://phc-roadmap.herokuapp.com',
